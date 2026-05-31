1476e756f560c189c0ffb3aacf2e661137e0b324
Rafael Woronkoff Montanha <rflmontanha@gmail.com>
2026-05-31 13:17:00 +0200

    Add create task endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -30,6 +30,25 @@ def home():
 def get_tasks():
     return jsonify(tasks)
 
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    data = request.get_json()
+
+    if not data or "title" not in data:
+        return jsonify({
+            "error": "Title is required"
+        }), 400
+    
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": data["title"],
+        "completed": False
+    }
+
+    tasks.append(new_task)
+
+    return jsonify(new_task), 201
+
 @app.route("/tasks/<int:task_id>")
 def get_task_by_id(task_id):
     for task in tasks:
@@ -39,8 +58,6 @@ def get_task_by_id(task_id):
     return jsonify({
         "error": "Task not found"
     }), 404
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
